872c32f2c764c19d861c08a1fbadc251c52735f5
MaxJaq <maxime.jaquard@gmail.com>
2020-03-24 23:10:49 +0100

    Youtube + Login

--- app/src/main/java/com/esilv/project_greiner_jaquard/MainActivity.java
+++ app/src/main/java/com/esilv/project_greiner_jaquard/MainActivity.java
@@ -6,15 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private Button main_button;
+    private Button loginButton;
+    private EditText usernameText,passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        main_button = (Button) findViewById(R.id.main_button);
-        main_button.setOnClickListener(new View.OnClickListener() {
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent youtubeActivity = new Intent(MainActivity.this, YoutubeActivity.class);
