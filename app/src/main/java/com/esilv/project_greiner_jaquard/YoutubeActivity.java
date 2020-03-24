ecc1698d788b3949d6920a0862eb443f29a9020d
Alescro <alexandre.greiner1@gmail.com>
2020-03-24 20:56:25 +0100

    Revert "Revert "youtube API""
    This reverts commit 57477a848ddaabbd7dcf498f274126153c94f67d.

---
+++ app/src/main/java/com/esilv/project_greiner_jaquard/YoutubeActivity.java
@@ -0,0 +1,14 @@
+package com.esilv.project_greiner_jaquard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YoutubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+    }
+}
