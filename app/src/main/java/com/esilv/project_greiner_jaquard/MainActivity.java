f9d25d20c0f458c4e1b21d20292867e320c31b26
Alescro <alexandre.greiner1@gmail.com>
2020-03-24 20:57:52 +0100

    Revert "Youtube API"
    This reverts commit 54838f2431e7733a74dbc17b67378c1f4266125a.

--- app/src/main/java/com/esilv/project_greiner_jaquard/MainActivity.java
+++ app/src/main/java/com/esilv/project_greiner_jaquard/MainActivity.java
@@ -2,24 +2,13 @@ package com.esilv.project_greiner_jaquard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button main_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        main_button = (Button) findViewById(R.id.main_button);
-        main_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent youtubeActivity = new Intent(MainActivity.this, YoutubeActivity.class);
-                startActivity(youtubeActivity);
-            }
-        });
     }
 }
