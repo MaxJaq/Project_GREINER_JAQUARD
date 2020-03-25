0e4ed04b73ff54170e26748b016abfc56f639cf5
MaxJaq <maxime.jaquard@gmail.com>
2020-03-25 17:25:28 +0100

    youtube fini

---
+++ app/src/main/java/com/esilv/project_greiner_jaquard/SecondActivity.java
@@ -0,0 +1,130 @@
+package com.esilv.project_greiner_jaquard;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import okhttp3.Challenge;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    private TextView name, email;
+    private Button signOut,youtubeButton;
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        name = (TextView) findViewById(R.id.textName);
+        email = (TextView) findViewById(R.id.textEmail);
+        signOut = (Button) findViewById(R.id.sign_out_button);
+        youtubeButton = (Button) findViewById(R.id.youtube_button);
+
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_out_button:
+                        signOut();
+                        break;
+                }
+
+            }
+        });
+        youtubeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this,YoutubeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            name.setText(personName);
+            email.setText(personEmail);
+        }
+
+        //Initialize and Assign Variables
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_home:
+
+                        return true;
+
+                    case R.id.nav_challenges:
+                        startActivity(new Intent(getApplicationContext()
+                                , YoutubeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.nav_score:
+                        startActivity(new Intent(getApplicationContext()
+                        , Score.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+        }
+
+
+
+
+
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(SecondActivity.this, "Signed out successfully !", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+
+    }
+}
